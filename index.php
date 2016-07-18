03127db9808655c353580782d734eec5956bff3f
Dzianis Zhaunerchyk <evrenior@mail.ru>
2016-07-18 16:29:59 -0400

    First version of my notebook

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Notebook app</title>
+	<script src="./js/jquery.min.js"> </script>
+
+	<link rel="stylesheet" href="./booklet/jquery.booklet.latest.css">
+	<link rel="stylesheet"  href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- jQuery -->
+	<script> window.jQuery || document.write('<script src="booklet/jquery-2.1.0.min.js"><\/script>') </script>
+
+	<!-- jQuery UI (optional) -->
+	<script src="./js/jquery-ui.min.js"></script>
+	<script> window.jQuery.ui || document.write('<script src="booklet/jquery-ui-1.10.4.custom.min.js"><\/script>') </script>
+
+	<!-- Booklet -->
+	<script src="booklet/jquery.easing.1.3.js"></script>
+	<script src="booklet/jquery.booklet.latest.min.js"></script>
+
+</head>
+<body>	
+	<div id="mybook">
+	    <div class="page">
+	        	<div class="expandingArea">
+		<pre><span></span><br></pre>
+		<textarea></textarea>
+		</div>
+	    </div>
+	    <div class="page">
+		<div class="expandingArea">
+		<pre><span></span><br></pre>
+		<textarea></textarea>
+		</div>
+	    </div>
+	    <div class="page">
+		<div class="expandingArea">
+		<pre><span></span><br></pre>
+		<textarea></textarea>
+		</div>
+	    </div>
+	    <div class="page">
+		<div class="expandingArea">
+		<pre><span></span><br></pre>
+		<textarea></textarea>
+		</div>	        
+	    </div>
+	    <div class="page">
+		<div class="expandingArea">
+		<pre><span></span><br></pre>
+		<textarea></textarea>
+		</div>
+	    </div>
+	</div>
+	<script src="js/main.js"></script>
+</body>
+</html>
